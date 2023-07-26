151bedb4c23f093218a546242dc11d051cf0d6f8
vijay.thakur <vijay.thakur@LAPTOP-I6QVIHGG>
2023-07-26 16:01:22 +0530

    Testing Version - 1

---
+++ GetApi/src/main/java/com/qa/client/RestClient.java
@@ -0,0 +1,18 @@
+package com.qa.client;
+
+import java.io.IOException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class RestClient {
+	// 1. Get Method
+	public CloseableHttpResponse get(String url) throws ClientProtocolException, IOException {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		HttpGet httpget = new HttpGet(url); // http get request
+		CloseableHttpResponse closeableHttpResponse = httpClient.execute(httpget); // hit the get URL	
+		return closeableHttpResponse;
+	}
+}
---
+++ GetApi/src/test/java/com/qa/tests/GetAPITest.java
@@ -0,0 +1,70 @@
+package com.qa.tests;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import org.apache.http.Header;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import com.qa.client.RestClient;
+import com.qa.util.TestUtils;
+
+public class GetAPITest {
+	RestClient restClient;
+	CloseableHttpResponse closeableHttpResponse;
+
+	@BeforeMethod
+	@DataProvider
+	public Object[][] getURLAPI() {
+		Object data[][] = TestUtils.getTestdata("GetURL");
+		return data;
+	}
+
+	@Test(dataProvider = "getURLAPI")
+	public void getsearchcompanyTest(String URL) throws ClientProtocolException, IOException 
+	{
+		restClient = new RestClient();
+		closeableHttpResponse = restClient.get(URL);
+
+		// a.Status Code:
+		int statuscode = closeableHttpResponse.getStatusLine().getStatusCode();
+		System.out.println("\n");
+		System.out.println("<================================================================>");
+		System.out.println("Status Code======>" + statuscode);
+		Assert.assertEquals(statuscode, 200, "Status code is not equal 200");
+
+		// b. JSON String:
+		String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
+
+		JSONObject responseJson = new JSONObject(responseString);
+		System.out.println("\n");
+		System.out.println("The URL is======>" + URL);
+		System.out.println("Response JSON from API======>" + responseJson);
+		//Assert.assertEquals(responseString, Website, "Website is not equal");
+		
+		// c. All Headers:
+		Header[] headersArray = closeableHttpResponse.getAllHeaders();
+
+		HashMap<String, String> allHeaders = new HashMap<String, String>();
+
+		for (Header header : headersArray) {
+			allHeaders.put(header.getName(), header.getValue());
+		}
+		System.out.println("\n");
+		System.out.println("Headers Array=====>" + allHeaders);
+		System.out.println("<================================================================>");
+		// System.out.println("\n");
+		FileWriter file=new FileWriter(System.getProperty("user.dir") + "\\src\\main\\java\\com\\qa\\config\\output.txt",true);
+		file.write("The URL is===>"+URL);
+		file.write("\n");
+		file.write("The Response JSON is===>"+responseString);
+		file.write("\n\n");
+		file.close();
+	}	
+}
